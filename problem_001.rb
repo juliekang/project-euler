92a6dcdf2804a5dfdf26670dba084bb1e255336d
Julie Kang <julie.kang@stanfordalumni.org>
2013-07-29 21:05:24 -0700

    Fix whitespace.

--- problem_001.rb
+++ problem_001.rb
@@ -2,11 +2,11 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 def compute_sum_of_threes_and_fives
-	sum = 0
-	(1...1000).each do |n|
-	  sum += n if n % 3 == 0 || n % 5 == 0
-	end
-	sum
+    sum = 0
+    (1...1000).each do |n|
+      sum += n if n % 3 == 0 || n % 5 == 0
+    end
+    sum
 end
 
 puts compute_sum_of_threes_and_fives
