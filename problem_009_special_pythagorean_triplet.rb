8d17922dfc90beb6a5bf9a8b963520d23ba9c282
Julie Kang <julie.kang@stanfordalumni.org>
2013-07-30 07:40:16 -0700

    Solution #9

--- problem_009_special_pythagorean_triplet.rb
+++ problem_009_special_pythagorean_triplet.rb
@@ -2,8 +2,17 @@
 # A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
 # a^2 + b^2 = c^2
-# For example, 32 + 42 = 9 + 16 = 25 = 52.
 
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
+def pythagorean_triplet
+  (1..1000).each do |a|
+    (a+1..1000).each do |b|
+      c = 1000 - b - a
+      return [a, b, c].inject(:*) if (c**2 == b**2 + a**2 && a + b + c == 1000)
+    end
+  end
+end
+
+puts pythagorean_triplet
